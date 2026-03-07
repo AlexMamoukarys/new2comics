cd7fcf828386ef3d6a0d0bb4196b1749330e9ff3
edwynzhou <edwyn.zhou@mail.utoronto.ca>
2026-03-07 17:38:22 -0500

    Update user controller

--- src/main/java/com/example/cms/controller/UserController.java
+++ src/main/java/com/example/cms/controller/UserController.java
@@ -9,20 +9,99 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.cms.model.entity.User;
+import com.example.cms.model.entity.Genre;
+import com.example.cms.model.entity.Power;
+import com.example.cms.model.entity.Publisher;
+import com.example.cms.model.entity.Team;
+import com.example.cms.model.entity.Volume;
+import com.example.cms.model.entity.Character;
+import com.example.cms.model.entity.LikedVolume;
+import com.example.cms.model.entity.SavedVolume;
+import com.example.cms.model.entity.PreferredGenre;
+import com.example.cms.model.entity.PreferredCharacter;
+import com.example.cms.model.entity.PreferredPublisher;
+import com.example.cms.model.entity.PreferredPower;
+import com.example.cms.model.entity.PreferredTeam;
 import com.example.cms.model.repository.UserRepository;
+import com.example.cms.model.repository.VolumeRepository;
+import com.example.cms.model.repository.CharacterRepository;
+import com.example.cms.model.repository.GenreRepository;
+import com.example.cms.model.repository.LikedVolumeRepository;
+import com.example.cms.model.repository.PowerRepository;
+import com.example.cms.model.repository.SavedVolumeRepository;
+import com.example.cms.model.repository.TeamRepository;
+import com.example.cms.model.repository.PreferredGenreRepository;
+import com.example.cms.model.repository.PreferredCharacterRepository;
+import com.example.cms.model.repository.PreferredPublisherRepository;
+import com.example.cms.model.repository.PreferredPowerRepository;
+import com.example.cms.model.repository.PreferredTeamRepository;
+import com.example.cms.model.repository.PublisherRepository;
 
 @CrossOrigin
 @RestController
 public class UserController {
     @Autowired
     private final UserRepository repository;
+    @Autowired
+    private final LikedVolumeRepository likedVolumeRepository;
+    @Autowired
+    private final SavedVolumeRepository savedVolumeRepository;
+    @Autowired
+    private final PreferredGenreRepository preferredGenreRepository;
+    @Autowired
+    private final PreferredCharacterRepository preferredCharacterRepository;
+    @Autowired
+    private final PreferredPublisherRepository preferredPublisherRepository;
+    @Autowired
+    private final PreferredPowerRepository preferredPowerRepository;
+    @Autowired
+    private final PreferredTeamRepository preferredTeamRepository;
+    @Autowired
+    private final VolumeRepository volumeRepository;
+    @Autowired
+    private final GenreRepository genreRepository;
+    @Autowired
+    private final CharacterRepository characterRepository;
+    @Autowired
+    private final PublisherRepository publisherRepository;
+    @Autowired
+    private final PowerRepository powerRepository;
+    @Autowired
+    private final TeamRepository teamRepository;
 
-    public UserController(UserRepository repository) {
+    public UserController(UserRepository repository, 
+                         LikedVolumeRepository likedVolumeRepository,
+                         SavedVolumeRepository savedVolumeRepository,
+                         PreferredGenreRepository preferredGenreRepository,
+                         PreferredCharacterRepository preferredCharacterRepository,
+                         PreferredPublisherRepository preferredPublisherRepository,
+                         PreferredPowerRepository preferredPowerRepository,
+                         PreferredTeamRepository preferredTeamRepository,
+                         VolumeRepository volumeRepository,
+                         GenreRepository genreRepository,
+                         CharacterRepository characterRepository,
+                         PublisherRepository publisherRepository,
+                         PowerRepository powerRepository,
+                         TeamRepository teamRepository) {
         this.repository = repository;
+        this.likedVolumeRepository = likedVolumeRepository;
+        this.savedVolumeRepository = savedVolumeRepository;
+        this.preferredGenreRepository = preferredGenreRepository;
+        this.preferredCharacterRepository = preferredCharacterRepository;
+        this.preferredPublisherRepository = preferredPublisherRepository;
+        this.preferredPowerRepository = preferredPowerRepository;
+        this.preferredTeamRepository = preferredTeamRepository;
+        this.volumeRepository = volumeRepository;
+        this.genreRepository = genreRepository;
+        this.characterRepository = characterRepository;
+        this.publisherRepository = publisherRepository;
+        this.powerRepository = powerRepository;
+        this.teamRepository = teamRepository;
     }
 
     @GetMapping("/users")
@@ -35,20 +114,111 @@ public class UserController {
         return repository.save(newUser);
     }
 
-    // GET liked volumes
-    // GET saved volumes
-    // GET publishers
-    // GET characters
-    // GET genres
-    // GET powers
-    // GET teams
-    // PUT publishers
-    // PUT characters
-    // PUT genres
-    // PUT liked volumes
-    // PUT saved volumes
-    // PUT powers
-    // PUT teams
-    // POST user
+    // GET endpoints
+    @GetMapping("/users/{id}/likedvolumes")
+    List<LikedVolume> getLikedVolumes(@PathVariable("id") Long userId) {
+        User user = repository.findById(userId).orElseThrow();
+        return user.getLikedVolumes();
+    }
+
+    @GetMapping("/users/{id}/savedvolumes")
+    List<SavedVolume> getSavedVolumes(@PathVariable("id") Long userId) {
+        User user = repository.findById(userId).orElseThrow();
+        return user.getSavedVolumes();
+    }
+
+    @GetMapping("/users/{id}/preferredpublishers")
+    List<PreferredPublisher> getPreferredPublishers(@PathVariable("id") Long userId) {
+        User user = repository.findById(userId).orElseThrow();
+        return user.getPreferredPublishers();
+    }
+
+    @GetMapping("/users/{id}/preferredcharacters")
+    List<PreferredCharacter> getPreferredCharacters(@PathVariable("id") Long userId) {
+        User user = repository.findById(userId).orElseThrow();
+        return user.getPreferredCharacters();
+    }
+
+    @GetMapping("/users/{id}/preferredgenres")
+    List<PreferredGenre> getPreferredGenres(@PathVariable("id") Long userId) {
+        User user = repository.findById(userId).orElseThrow();
+        return user.getPreferredGenres();
+    }
+
+    @GetMapping("/users/{id}/preferredpowers")
+    List<PreferredPower> getPreferredPowers(@PathVariable("id") Long userId) {
+        User user = repository.findById(userId).orElseThrow();
+        return user.getPreferredPowers();
+    }
+
+    @GetMapping("/users/{id}/preferredteams")
+    List<PreferredTeam> getPreferredTeams(@PathVariable("id") Long userId) {
+        User user = repository.findById(userId).orElseThrow();
+        return user.getPreferredTeams();
+    }
+
+    // PUT endpoints
+    @PutMapping("/users/{userId}/preferredpublishers/{publisherId}")
+    PreferredPublisher addPreferredPublisher(@PathVariable("userId") Long userId, 
+                                             @PathVariable("publisherId") Long publisherId) {
+        User user = repository.findById(userId).orElseThrow();
+        Publisher publisher = publisherRepository.findById(publisherId).orElseThrow();
+        PreferredPublisher preferredPublisher = new PreferredPublisher(user, publisher);
+        return preferredPublisherRepository.save(preferredPublisher);
+    }
+
+    @PutMapping("/users/{userId}/preferredcharacters/{characterId}")
+    PreferredCharacter addPreferredCharacter(@PathVariable("userId") Long userId, 
+                                             @PathVariable("characterId") Long characterId) {
+        User user = repository.findById(userId).orElseThrow();
+        Character character = characterRepository.findById(characterId).orElseThrow();
+        PreferredCharacter preferredCharacter = new PreferredCharacter(user, character);
+        return preferredCharacterRepository.save(preferredCharacter);
+    }
+
+    @PutMapping("/users/{userId}/preferredgenres/{genreId}")
+    PreferredGenre addPreferredGenre(@PathVariable("userId") Long userId, 
+                                     @PathVariable("genreId") Long genreId) {
+        User user = repository.findById(userId).orElseThrow();
+        Genre genre = genreRepository.findById(genreId).orElseThrow();
+        PreferredGenre preferredGenre = new PreferredGenre(user, genre);
+        return preferredGenreRepository.save(preferredGenre);
+    }
+
+    @PutMapping("/users/{userId}/likedvolumes/{volumeId}")
+    LikedVolume addLikedVolume(@PathVariable("userId") Long userId, 
+                               @PathVariable("volumeId") Long volumeId) {
+        User user = repository.findById(userId).orElseThrow();
+        Volume volume = volumeRepository.findById(volumeId).orElseThrow();
+        LikedVolume likedVolume = new LikedVolume(user, volume);
+        return likedVolumeRepository.save(likedVolume);
+    }
+
+    @PutMapping("/users/{userId}/savedvolumes/{volumeId}")
+    SavedVolume addSavedVolume(@PathVariable("userId") Long userId, 
+                               @PathVariable("volumeId") Long volumeId) {
+        User user = repository.findById(userId).orElseThrow();
+        Volume volume = volumeRepository.findById(volumeId).orElseThrow();
+        SavedVolume savedVolume = new SavedVolume(user, volume);
+        return savedVolumeRepository.save(savedVolume);
+    }
+
+    @PutMapping("/users/{userId}/preferredpowers/{powerId}")
+    PreferredPower addPreferredPower(@PathVariable("userId") Long userId, 
+                                     @PathVariable("powerId") Long powerId) {
+        User user = repository.findById(userId).orElseThrow();
+        Power power = powerRepository.findById(powerId).orElseThrow();
+        PreferredPower preferredPower = new PreferredPower(user, power);
+        return preferredPowerRepository.save(preferredPower);
+    }
+
+    @PutMapping("/users/{userId}/preferredteams/{teamId}")
+    PreferredTeam addPreferredTeam(@PathVariable("userId") Long userId, 
+                                   @PathVariable("teamId") Long teamId) {
+        User user = repository.findById(userId).orElseThrow();
+        Team team = teamRepository.findById(teamId).orElseThrow();
+        PreferredTeam preferredTeam = new PreferredTeam(user, team);
+        return preferredTeamRepository.save(preferredTeam);
+    }
 
 }
