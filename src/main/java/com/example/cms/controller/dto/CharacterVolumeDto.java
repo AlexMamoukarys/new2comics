6bb36b1cccae04a6d2eed81c03f954796c9d1e8d
SeanYeSchool <sean171171school@gmail.com>
2026-03-09 23:28:50 -0400

    actually modified

--- src/main/java/com/example/cms/controller/dto/CharacterVolumeDto.java
+++ src/main/java/com/example/cms/controller/dto/CharacterVolumeDto.java
@@ -1,4 +1,12 @@
 package com.example.cms.controller.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Getter
+@Setter
 public class CharacterVolumeDto {
+    private Long characterId;
+    private Long volumeId;
 }
