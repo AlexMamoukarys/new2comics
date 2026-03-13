083ed5592c630c49a8576865d6a6b2082569e0f7
Renzou756 <renzo.ugarte@mail.utoronto.ca>
2026-03-13 03:48:56 -0400

    Genre PUT

--- src/main/java/com/example/cms/controller/GenreController.java
+++ src/main/java/com/example/cms/controller/GenreController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -45,7 +46,7 @@ public class GenreController {
         repository.deleteById(genreId);
     }
 
-    @PostMapping("/genres/changeName/{id}/{name}")                 // Update
+    @PutMapping("/genres/changeName/{id}/{name}")                 // Update
     void changeName(@PathVariable("id") long id, @PathVariable("name") String name) {
         repository.changeName(id, name);
     }
