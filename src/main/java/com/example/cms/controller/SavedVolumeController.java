b0aa185d6c8837ccfb4669fe803217844fe88749
SeanYeSchool <sean171171school@gmail.com>
2026-04-03 12:56:57 -0400

    Renamed to createSavedVolume

--- src/main/java/com/example/cms/controller/SavedVolumeController.java
+++ src/main/java/com/example/cms/controller/SavedVolumeController.java
@@ -30,7 +30,7 @@ public class SavedVolumeController {
     }
 
     @PostMapping("/savedvolumes")
-    SavedVolume createLikedVolume(@RequestBody SavedVolume newVolume) {
+    SavedVolume createSavedVolume(@RequestBody SavedVolume newVolume) {
         return repository.save(newVolume);
     }
 
