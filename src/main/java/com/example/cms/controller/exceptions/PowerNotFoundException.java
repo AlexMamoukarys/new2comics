24ac6f71d932dd3818f3e97ab389ee548570c333
edwynzhou <edwyn.zhou@mail.utoronto.ca>
2026-04-03 10:09:38 -0400

    Remove powers

--- src/main/java/com/example/cms/controller/exceptions/PowerNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.cms.controller.exceptions;
-
-public class PowerNotFoundException extends RuntimeException {
-    public PowerNotFoundException(Long powerId) {
-        super("Could not find power " + powerId);
-    }
-}
